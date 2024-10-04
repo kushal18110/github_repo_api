054de55fdeac52770378f477ec0e2f270fd131fd
kushal <kushalparekh2003@gmail.com>
2024-10-04 21:51:34 +0530

    first commit

--- github_repo_api/ContentView.swift
+++ github_repo_api/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var searchText: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                VStack (alignment: .center) {
+                    Circle()
+                        .frame(width: 100, height: 100)
+                        .padding(.top, 5)
+                    
+                    Text("user name")
+                        .font(.largeTitle)
+                    
+                    Text("bio")
+                        .font(.title2)
+                        .padding(.top, 5)
+                }
+                .frame(maxWidth: .infinity)
+                
+                Section(header: Text("Repositories")) {
+                    ForEach(0...10, id: \.self){item in
+                        repoDetails()
+                    }
+                }
+                
+            }
+            .searchable(text: $searchText)
         }
-        .padding()
+    }
+    
+    @ViewBuilder
+    func repoDetails() -> some View {
+        VStack (alignment: .leading,  spacing: 7) {
+            Text("Repository name")
+            
+            Text("Description")
+                .lineLimit(2)
+            
+            Text("created at")
+                .font(.caption)
+            
+            HStack (spacing: 20){
+                HStack {
+                    Image(systemName: "star")
+                        .foregroundStyle(Color.orange)
+                    Text("\(Int.random(in: 1...10))")
+                }
+                
+                HStack {
+                    Image(systemName: "eye")
+                        .foregroundStyle(Color.gray)
+                    Text("\(Int.random(in: 1...20))")
+                }
+            }
+        }
     }
 }
 
